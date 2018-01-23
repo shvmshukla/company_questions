fac63f6a708222a286794cf6e6929ebd326a6966
SHIVAM SHUKLA <shvmshukla37@gmail.com>
2018-01-23 10:09:13 +0530

    Update anti digonals of N*N matrix.java

--- anti digonals of N*N matrix.java
+++ anti digonals of N*N matrix.java
@@ -1,4 +1,20 @@
 //program to print anti digonals 
+Input: 	
+
+1 2 3
+4 5 6
+7 8 9
+
+Return the following :
+
+[ 
+  [1],
+  [2, 4],
+  [3, 5, 7],
+  [6, 8],
+  [9]
+]
+
 
 public class Solution {
 	public ArrayList<ArrayList<Integer>> diagonal(ArrayList<ArrayList<Integer>> a) {
