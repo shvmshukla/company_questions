d1e19c40c43b5f0b7de0d86010cb3edfd72d37df
SHIVAM SHUKLA <shvmshukla37@gmail.com>
2018-01-25 00:19:15 +0530

    Create plus_1.java

---
+++ plus_1.java
@@ -0,0 +1,25 @@
+/**
+ *
+ * @author shivam
+ */
+public class plus1 {
+    public static void main(String[] args) {
+        
+    
+    
+        int arr[]={0};
+        int len=arr.length;
+        System.out.println("shivam");
+       String s="";
+       int i=0;
+       while(i<len)
+       {
+           s+=arr[i];
+           i++;
+       }
+      int temp= Integer.parseInt(s)+1  ;
+        System.out.println(temp);
+    }
+
+    
+}
