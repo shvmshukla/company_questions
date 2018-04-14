12b4df6eabaf0be9b01c4394dcf587d9d1905e34
SHIVAM SHUKLA <shvmshukla37@gmail.com>
2018-04-15 02:51:21 +0530

    Create Find triplets with zero sum(google).java

---
+++ Find triplets with zero sum(google).java
@@ -0,0 +1,48 @@
+
+/*
+Given an array A[] of n elements. 
+The task is to complete the function which returns true if triplets exists in array A[] whose sum is zero else returns false.
+Example(To be used only for expected output) :
+Input:
+2
+5
+0 -1 2 -3 1
+3
+1 2 3
+Output:
+1
+0
+*/
+
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+/*Complete the function below*/
+class GfG
+{
+	public boolean findTriplets(int arr[] , int n)
+       {
+         Arrays.sort(arr);
+		int count = 0;
+		// -3 -2 -1 1 2 3
+		for (int j = 0; j <= n - 2; j++) {
+			int a = j + 1;
+			int b = n - 1;
+			while (a < b) {
+				if (arr[a] + arr[b] == -arr[j]) {
+					count++;
+					break;
+					// a++;
+					// b--;
+				} else if (arr[a] + arr[b] < -arr[j]) {
+					a++;
+				} else {
+					b--;
+				}
+
+			}
+		}
+		 return count > 0 ? true:false;
+       }
+}
