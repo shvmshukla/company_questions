1a348d2086b07b3b5784c614e21b21c7a1f911c5
SHIVAM SHUKLA <shvmshukla37@gmail.com>
2018-05-25 09:49:47 +0530

    Create move_zeros_to_end.java

---
+++ move_zeros_to_end.java
@@ -0,0 +1,38 @@
+
+/*
+Given an array, move the zeros present in it to the tail of array.
+*/
+
+/**
+ *
+ * @author shivam
+ */
+public class move_zeros_to_end {
+    public static void main(String[] args) {
+        int[] arr={1,2,0,0,3,4,0,0,1,0};
+       int slow=0,fast=0;
+       while(fast<arr.length)
+       {
+           if(arr[fast]==0 )
+           {
+               fast++;
+               continue;
+           }
+          arr[slow]=arr[fast];
+          slow++;
+          fast++;
+          
+       }
+    
+    //fill remaining pos from slow to arr.length by 0   
+       for(int i=slow;i<arr.length;i++) 
+      {
+          arr[i]=0;
+      }
+       //printinf array after moving zeros to end 
+       for(int i=0;i<arr.length;i++) 
+      {
+          System.out.print(arr[i]+" ");
+      }    
+    }
+}
