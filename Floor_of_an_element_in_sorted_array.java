20cadd9b673e1199fb9736aa62f579c4e0e00cd5
SHIVAM SHUKLA <shvmshukla37@gmail.com>
2018-06-04 15:11:46 +0530

    Update Floor_of_an_element_in_sorted_array.java

--- Floor_of_an_element_in_sorted_array.java
+++ Floor_of_an_element_in_sorted_array.java
@@ -1,5 +1,7 @@
 /*
-Given a sorted array and a value x, the ceiling of x is the smallest element in array greater than or equal to x, and the floor is the greatest element smaller than or equal to x. Assume than the array is sorted in non-decreasing order. Write efficient functions to find floor and ceiling of x.
+Given a sorted array and a value x, the ceiling of x is the smallest element in array greater than or equal to x, 
+and the floor is the greatest element smaller than or equal to x. Assume than the array is sorted in non-decreasing order. 
+Write efficient functions to find floor and ceiling of x.
 
 Examples :
 
