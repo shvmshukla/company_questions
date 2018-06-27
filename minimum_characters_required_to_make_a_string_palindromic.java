89bb38446e379a21371161e495ddfec1f48bbb60
SHIVAM SHUKLA <shvmshukla37@gmail.com>
2018-06-28 03:17:15 +0530

    Create minimum_characters_required_to_make_a_string_palindromic.java

---
+++ minimum_characters_required_to_make_a_string_palindromic.java
@@ -0,0 +1,42 @@
+/*
+You are given a string. The only operation allowed is to insert characters in the beginning of the string. How many minimum characters are needed to be inserted to make the string a palindrome string
+
+Example:
+Input: ABC
+Output: 2
+Input: AACECAAAA
+Output: 2
+*/
+---------------------------------------------------------------------------------------------------------------------------
+
+public class Solution {
+    boolean isPallindrome(String str)
+    {
+        for(int i=0;i<str.length();i++)
+        {
+            if(str.charAt(i)==str.charAt(str.length()-i-1))
+             continue;
+            else
+              return false;
+        }
+        
+        return true;
+    }
+    public int solve(String A) {
+        StringBuffer sb= new StringBuffer(A);
+        int count=0;
+        while(sb.length()>0)
+        {
+            if(isPallindrome(sb.toString())==false)
+             {
+                sb.deleteCharAt(sb.length()-1);
+                count++;
+             }
+             else
+             {
+                 break;
+             }
+        }
+    return count;    
+    }
+}
